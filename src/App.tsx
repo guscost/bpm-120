f0b8c906c52553e0dae608a935bb13d45e931e11
Gus Cost <guscost@gmail.com>
2024-07-15 15:02:42 -0400

    initial bpm detector setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import load from "audio-loader";
+import { analyze } from "web-audio-beat-detector";
 
 function App() {
+  const [bpm, setBpm] = useState(-1);
+  load(
+    "https://gus.media/songs/nobody_knows_you_when_youre_down_and_out.mp3"
+  ).then(function (buffer: AudioBuffer) {
+    analyze(buffer)
+      .then((tempo: number) => {
+        setBpm(tempo);
+      })
+      .catch((err) => {
+        alert(err)
+      });
+    console.log(buffer); // => <AudioBuffer>
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          BPM: <code>{bpm}</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
